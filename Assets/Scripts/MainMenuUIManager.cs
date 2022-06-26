9bbe20a0383902aa78e6c5107564916fe40d7bc6
Stroudle <julia.stopassola.moura@gmail.com>
2022-06-25 23:06:17 -0300

    Basic Main Menu Screens

---
+++ Assets/Scripts/MainMenuUIManager.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using TMPro;
+
+public class MainMenuUIManager : MonoBehaviour
+{
+    [SerializeField] private GameObject title, startButton, howToPlayButton;
+
+
+    public void OpenHowToPlayUI()
+    {
+
+    }
+}
